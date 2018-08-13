4ea9af134b97de6b19904f7c5be9e8e773325678
shaw <gggshaw007@gmail.com>
2018-08-14 00:40:44 +0800

    handle post

--- Node/server.js
+++ Node/server.js
@@ -3,12 +3,24 @@ var url = require("url");
 
 function start(route,handle){
   http.createServer(function(request,response){
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for"+pathname+" received.");
+    console.log("Request for " + pathname + " received.");
     
-    var content = route(handle, pathname, response);
+    request.setEncoding("utf-8");
 
-    console.log("Request received")
+    request.addListener("data",function(postDataChunk){
+      postData += postDataChunk;
+      console.log("Received POST data chunk "+
+      postDataChunk+".");
+    });
+
+    request.addListener("end",function(){
+      route(handle,pathname,response,postData)
+    })
+    //var content = route(handle, pathname, response);
+    
+    //console.log("Request received")
     // response.writeHead(200,{"Content-Type":"text/plain"});
     // response.write(content);
     // response.end(); 
