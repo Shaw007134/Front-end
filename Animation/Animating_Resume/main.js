374269f0eb9c1090bc72a94a0d95ffac8495cd25
shaw <gggshaw007@gmail.com>
2018-11-14 17:08:49 +0800

    add animating resume

---
+++ Animation/Animating_Resume/main.js
@@ -0,0 +1,158 @@
+// var Prism = require('prismjs')
+
+var data1 =`
+  /* 
+ * 面试官你好，我是XXX
+ * 只用文字作做我介绍太单调了
+ * 我就用代码来介绍吧
+ * 首先准备一些样式
+ */
+{
+  transition: all 1s;
+}
+html{
+  background: #eee;
+}
+#code{
+  border: 1px solid #aaa;
+  padding: 16px;
+}
+/* 我需要一点代码高亮 */
+.token.selector{ color: #690; }
+.token.property{ color: #905; }
+/* 加一个呼吸效果 */
+#code{
+  animation: breath 0.5s infinite alternate-reverse;
+}
+/* 现在正式开始 */
+/* 我需要一张白纸 */
+#code-wrapper{
+  width: 50%; left: 0; position: fixed; 
+  height: 100%;
+}
+#paper > .content {
+ display: block;
+}
+/* 于是我就可以在白纸上写字了，请看右边 */
+
+`
+
+var data2 =`
+/* 接下来用一个优秀的库 marked.js
+ * 把 Markdown 变成 HTML
+ */
+`
+
+
+var md = `
+# 自我介绍
+我叫 XXX
+1990 年 1 月出生
+XXX 学校毕业
+自学前端半年
+希望应聘前端开发岗位
+# 技能介绍
+熟悉 JavaScript CSS
+# 项目介绍
+1. XXX 轮播
+2. XXX 简历
+3. XXX 画板
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+# 联系方式
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+`
+let css3 = `
+/*
+ * 这就是我的会动的简历
+ * 谢谢观看
+ */
+`
+
+
+function write(data, id, type, highlight){
+  return new Promise(function(resolve){
+    console.log("write to "+id)
+    id = $(id)
+    if(highlight){
+      settime(data,write_highlight,id,resolve)
+    }else{
+      settime(data,write_plain,id,resolve)
+    }
+
+    function write_highlight(code){
+      id.html(Prism.highlight(code,window['Prism'].languages[type]))
+    }
+
+    function write_plain(code){
+      id.html(code)
+    }
+
+
+  })
+  // return new Promise(resolve)  
+}
+
+function settime(data,fun,id,resolve){
+  var i = 0
+  let timer = setInterval(()=>{
+    fun(data.substr(0,i))
+    id.scrollTop(id[0].scrollHeight)
+    i = i+1
+    if(i>data.length){
+      clearInterval(timer)
+      resolve && resolve.call()
+    }
+  },10)
+}
+
+function createPaper(){
+  return new Promise(function(resolve){
+    var paper = document.createElement('div') 
+    paper.id = 'paper'
+    var content = document.createElement('pre')
+    content.className = 'content'
+    content.id = 'content'
+    paper.appendChild(content)
+    document.body.appendChild(paper)
+    resolve && resolve.call()
+  })
+}
+
+// var write_css = write(data1,"#style_m")
+write(data1,"#code",'css',true).then
+  (write(data1,"#style_m",'css','')).then
+  (createPaper).then
+  (write(md,"#content",'markup',true))
+
+
+
+
