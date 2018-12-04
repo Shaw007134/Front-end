b483ab7d9a19339e0c5bf5d9f5b72ef6bf31d757
shaw <gggshaw007@gmail.com>
2018-12-04 19:04:09 +0800

    add css less sass loader

--- Webpack-study/src/main.js
+++ Webpack-study/src/main.js
@@ -1,7 +1,9 @@
 import $ from 'jquery'
-
+import "./css/index.css"
+import "./css/index.less"
+import "./css/index.scss"
 $(function(){
-  $('li:odd').css('backgroundColor','orange')
+  $('li:odd').css('backgroundColor','red')
   $('li:even').css('backgroundColor',function(){
     return "#"+"C0CB41"
   })
--- Webpack-study/webpack.config.js
+++ Webpack-study/webpack.config.js
@@ -1,9 +1,38 @@
 const path = require('path')
+const wepback = require('webpack')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname,'./src/main.js'),
   output: {
     path: path.join(__dirname,'./dist'),
     filename: 'bundle.js'
+  },
+  /* devServer:{
+    open: true,
+    port: 3000,
+    contentBase: 'src',
+    hot: 'true
+   },*/
+  plugins:[
+    //new webpack.HotModuleReplacementPlugin(),
+    new htmlWebpackPlugin({
+      //根据指定路径，生成内存中的模板页面
+      //自动在页面中加载bundle.js
+      template: path.join(__dirname,'./src/index.html'),
+      filename: 'index.html'
+    })
+  ],
+  module:{
+    //这个节点，用于配置所有第三方模块加载器
+    rules:[
+      //所有第三方模块的匹配规则
+      {test: /\.css$/,use:['style-loader','css-loader']},
+      //配置处理.css文件的第三方匹配和规则
+      //loader调用是从右到左
+      {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+      //先调用less-loader处理，处理后交给css-loader处理，最后由style-loader处理样式
+      {test:/\.scss$/, use:['style-loader','css-loader','sass-loader']},
+    ]
   }
 };
